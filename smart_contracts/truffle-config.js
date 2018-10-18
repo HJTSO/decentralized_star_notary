15eb3a06934fd5ad609afe7408efd1592955c0dd
“HJT” <562784738@qq.com>
2018-10-19 00:53:38 +0900

    truffle compile

--- smart_contracts/truffle-config.js
+++ smart_contracts/truffle-config.js
@@ -11,8 +11,20 @@
  *     gasPrice: 10000000000,
  *   },
  */
-
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-};
+    // See <http://truffleframework.com/docs/advanced/configuration>
+    // to customize your Truffle configuration!
+      networks: { 
+        development: {
+          host: '127.0.0.1',
+          port: 8545,
+          network_id: "*"
+        }, 
+        rinkeby: {
+          provider: () => new HDWalletProvider(mnemonic, infura),
+          network_id: 4,
+          gas : 4500000,
+          gasPrice : 10000000000
+        },
+      }
+    };
